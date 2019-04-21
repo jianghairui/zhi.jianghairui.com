af38afd963473837d6b8b82fa7dbda7db078285e
JHR <1873645345@qq.com>
2019-04-22 00:17:57 +0800

    SECOND

--- application/api/config/app.php
+++ application/api/config/app.php
@@ -11,4 +11,12 @@ return array(
         //支持Html,Console
         'type'  =>  'html',
     ],
+
+    //博物馆商店商户号,公众号
+    'appid' => 'wx1ef69d837c12a709',
+    'app_secret' => 'b95d230bfbd56c0e1944e7f5d36b01f2',
+    'mch_id' => '1251831001',
+    'cert_path'  => '/var/www/zhi.jianghairui.com/public/cert/bwg_cert/apiclient_cert.pem',
+    'key_path'  => '/var/www/zhi.jianghairui.com/public/cert/bwg_cert/apiclient_key.pem',
+    'mch_key'  => '123654aaaZZZcaifu888aaazzzaaazzz',
 );
--- application/api/controller/Common.php
+++ application/api/controller/Common.php
@@ -13,8 +13,6 @@ class Common extends Controller {
     protected $cmd = '';
     protected $domain = '';
     protected $weburl = '';
-    protected $config = [];
-    protected $myinfo = [];
 
     public function initialize()
     {
@@ -22,25 +20,9 @@ class Common extends Controller {
         $this->cmd = request()->controller() . '/' . request()->action();
         $this->domain = 'zhi.jianghairui.com';
         $this->weburl = 'http://zhi.jianghairui.com/';
-        $this->config = [
-            'appid' => 'wxe7bbb3344157bd09',
-            'appsecret' => '7326d844c3399bc923a7aec7699fdd67',
-            'mch_id'             => '1490402642',
-            'key'                => 'TIANJINTAOCIYUAN20190111SHWHCOPY',
-        ];
+
     }
 
-
-    //Exception日志
-    protected function log($cmd,$str) {
-        $file= ROOT_PATH . '/exception_api.txt';
-        $text='[Time ' . date('Y-m-d H:i:s') ."]\ncmd:" .$cmd. "\n" .$str. "\n---END---" . "\n";
-        if(false !== fopen($file,'a+')){
-            file_put_contents($file,$text,FILE_APPEND);
-        }else{
-            echo '创建失败';
-        }
-    }
     //支付回调日志
     protected function paylog($cmd,$str) {
         $file= ROOT_PATH . '/notify.txt';
@@ -49,6 +31,42 @@ class Common extends Controller {
             file_put_contents($file,$text,FILE_APPEND);
         }else{
             echo '创建失败';
+        }
+    }
+
+    protected function gift_log($cmd = '',$msg = '',$device_num = '') {
+        $file= ROOT_PATH . '/giftlog/'.$device_num.'gift_log.txt';
+        $text='[Time ' . date('Y-m-d H:i:s') ."]  cmd:".$cmd."\n".$msg."\n---END---" . "\n";
+        if(false !== fopen($file,'a+')){
+            file_put_contents($file,$text,FILE_APPEND);
+        }else{
+            echo '创建失败';
+        }
+    }
+
+    protected function excep($cmd = '',$msg = '') {
+        $file= ROOT_PATH . '/exception.txt';
+        $text='[Time ' . date('Y-m-d H:i:s') ."]  cmd:".$cmd."\n".$msg."\n---END---" . "\n";
+        if(false !== fopen($file,'a+')){
+            file_put_contents($file,$text,FILE_APPEND);
+        }else{
+            echo '创建失败';
+        }
+    }
+
+    protected function asyn_smtp($data) {
+        $param = http_build_query($data);
+        $fp = fsockopen($this->domain, 80, $errno, $errstr, 20);
+        if (!$fp){
+            echo 'error fsockopen';
+        }else{
+            stream_set_blocking($fp,0);
+            $http = "GET /api/email/sendSmtp?".$param." HTTP/1.1\r\n";
+            $http .= "Host: ".$this->domain."\r\n";
+            $http .= "Connection: Close\r\n\r\n";
+            fwrite($fp,$http);
+            usleep(1000);
+            fclose($fp);
         }
     }
 
